14b4edf3c0c6fc9368eb741a9f7763482ae83456
yats1304 <chaubaly@gmail.com>
2025-08-22 20:43:26 +0530

    Hero Section done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,52 @@
 import styled from "styled-components";
+import { useEffect,useState } from "react";
+import SearchResult from "./components/SearchResult/SearchResult";
+
+export const BASE_URL = "http://localhost:9000";
 
 const App = () => {
-  return <Container>
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+
+  useEffect(() => {
+      const fetchFoodData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(BASE_URL);
+
+      const json =await response.json();
+
+      setData(json);
+      setLoading(false);
+    } catch (error) {
+      setError("Unable to fetch data");
+    }
+  };
+  fetchFoodData();
+  }, [])
+
+  console.log(data);
+
+  /* 
+  const temp = {
+        "name": "Boilded Egg",
+        "price": 10,
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        "image": "/images/egg.png",
+        "type": "breakfast"
+  }; 
+  */
+  
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>loading.....</div>;
+
+  return (
+  <>
+  <Container>
     <TopContainer>
       <div className="logo">
         <img src="/logo.svg" alt="logo"/>
@@ -16,12 +61,16 @@ const App = () => {
       <Button>Lunch</Button>
       <Button>Dinner</Button>
     </FilterContainer>
-  </Container>;
+  </Container>
+  
+  <SearchResult data={data}/>
+</>
+  );
 };
 
 export default App;
 
-const Container = styled.div`
+export const Container = styled.div`
  max-width: 1200px;
  margin: 0 auto;
 `;
@@ -49,13 +98,15 @@ const TopContainer = styled.section`
 const FilterContainer = styled.section`
   display: flex;
   justify-content: center;
-  gap: 12px ;
+  gap: 12px;
+  padding-bottom: 40px;
 `;
 
-const Button = styled.button`
+export const Button = styled.button`
   background-color: #FF4343;
   border-radius: 5px;
   padding: 6px 12px;
   border: none;
   color: white;
 `;
+
