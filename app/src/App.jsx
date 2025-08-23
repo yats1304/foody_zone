6f247e6a495ac1f563e7c5a2d1a0088109e0c521
yats1304 <chaubaly@gmail.com>
2025-08-23 08:32:53 +0530

    backend URL added

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { useEffect,useState } from "react";
 import SearchResult from "./components/SearchResult/SearchResult";
 
-export const BASE_URL = "http://localhost:9000";
+export const BASE_URL = "https://foody-zone-backend.vercel.app";
 
 const App = () => {
   const [data, setData] = useState(null);
